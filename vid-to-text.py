ac39fa7c8fc6976f8041a76cb07e380b71ecee92
Joaquín Sande <jsande@practia.uy>
2025-09-04 16:43:32 -0300

    color picker (1-3) plus some cleanup

--- vid-to-text.py
+++ vid-to-text.py
@@ -11,6 +11,23 @@ reader = easyocr.Reader(['en'])
 paused = False
 last_frame = None
 
+main_color = (255,0,0)
+
+def process_text(input_frame):
+    frame = input_frame.copy()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    results = reader.readtext(gray)
+    for (bbox, text, prob) in results:
+        (top_left, top_right, bottom_right, bottom_left) = bbox
+        top_left = tuple(map(int, top_left))
+        bottom_right = tuple(map(int, bottom_right))
+        cv2.rectangle(frame, top_left, bottom_right, main_color, 2)
+        cv2.putText(frame, text, (top_left[0], top_left[1] - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, main_color, 2)
+
+    cv2.imshow("Webcam OCR", frame)
+
+#does this make the window resizable?
 cv2.namedWindow("Webcam OCR", cv2.WINDOW_NORMAL)
 
 while True:
@@ -19,32 +36,34 @@ while True:
         if not ret:
             continue
         last_frame = frame.copy()
-        cv2.imshow("Webcam OCR", frame)
+        cv2.imshow("Webcam OCR", frame) 
 
-    key = cv2.waitKey(1) & 0xFF
+    key = cv2.waitKey(1) & 0xFF #because Windows <<<<< Linux or Mac, bitwise AND is needed
 
     if key == ord('q'):  # Quit
         break
 
-    elif key == 32 and not paused:  # Spacebar PRESSED
+    elif key == 32 and not paused:  # Spacebar
         paused = True
+        process_text(last_frame)
 
-        gray = cv2.cvtColor(last_frame, cv2.COLOR_BGR2GRAY)
-        results = reader.readtext(gray)
-
-        for (bbox, text, prob) in results:
-            (top_left, top_right, bottom_right, bottom_left) = bbox
-            top_left = tuple(map(int, top_left))
-            bottom_right = tuple(map(int, bottom_right))
-            cv2.rectangle(last_frame, top_left, bottom_right, (0, 255, 0), 2)
-            cv2.putText(last_frame, text, (top_left[0], top_left[1] - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-        # Show paused frame with OCR results
-        cv2.imshow("Webcam OCR", last_frame)
-
-    elif key != 255 and paused:  # Any key RELEASED (other than no key)
+    elif key == 32 and paused:  # Spacebar
         paused = False
+
+    elif key == ord('1'):
+        main_color = (255,0,0)
+        if paused:
+            process_text(last_frame)
+
+    elif key == ord('2'):
+        main_color = (0,255,0)
+        if paused:
+            process_text(last_frame)
+
+    elif key == ord('3'):
+        main_color = (0,0,255)
+        if paused:
+            process_text(last_frame)
 
 cap.release()
 cv2.destroyAllWindows()
